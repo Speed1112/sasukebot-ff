8b7d7fad9a9606f3951f3ac0f303eada2be54595
𝐓𝐇𝐔𝐍𝐃𝐄𝐑 𝐁𝐨𝐭 <sasuke.bot55@gmail.com>
2025-04-04 17:44:24 +0200

    Add files via upload

---
+++ plugins/الدعم-دعم.js
@@ -0,0 +1,22 @@
+let handler = async m => m.reply(`
+*❐═━━━═╊⊰🦇⊱╉═━━━═❐*
+≡  *TheRaizel-Bot-MD*
+─────────────
+_GROUP:_
+─────────────
+▢ *Account STEVEN*
+https://www.atom.bio/q_1
+─────────────
+▢ *SUPPORT_GROUP*
+https://chat.whatsapp.com/IiuQGbYg045JuVENBN2pRe
+─────────────
+▢ *YouTube*
+• https://www.youtube.com/@q1_1r
+─────────────
+*❐═━━━═╊⊰🦇⊱╉═━━━═❐*
+`.trim())
+handler.help = ['gpflash']
+handler.tags = ['main']
+handler.command = ['group', 'support','الدعم','دعم']
+
+export default handler
