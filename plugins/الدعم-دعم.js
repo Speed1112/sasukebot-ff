855376047a40ace62104c8adafa3e4ccc2febd4d
𝐓𝐇𝐔𝐍𝐃𝐄𝐑 𝐁𝐨𝐭 <sasuke.bot55@gmail.com>
2025-04-05 02:21:44 +0200

    Update الدعم-دعم.js

--- plugins/الدعم-دعم.js
+++ plugins/الدعم-دعم.js
@@ -1,22 +1,22 @@
 let handler = async m => m.reply(`
 *❐═━━━═╊⊰🦇⊱╉═━━━═❐*
-≡  *TheRaizel-Bot-MD*
+≡  *𝑺𝑨𝑺𝑼𝑲𝑬 𝑩𝑶𝑻*
 ─────────────
 _GROUP:_
 ─────────────
 ▢ *Account STEVEN*
-https://www.atom.bio/q_1
+https://whatsapp.com/channel/0029VakZ0PI2Jl8FfeibKR0e
 ─────────────
 ▢ *SUPPORT_GROUP*
-https://chat.whatsapp.com/IiuQGbYg045JuVENBN2pRe
+https://chat.whatsapp.com/E4MJjjQR4s250BreXu7G07
 ─────────────
 ▢ *YouTube*
-• https://www.youtube.com/@q1_1r
+• www.youtube.com/@thunderbotmd
 ─────────────
 *❐═━━━═╊⊰🦇⊱╉═━━━═❐*
 `.trim())
 handler.help = ['gpflash']
 handler.tags = ['main']
-handler.command = ['group', 'support','الدعم','دعم']
+handler.command = ['group', 'support','سورس','دعم']
 
 export default handler
